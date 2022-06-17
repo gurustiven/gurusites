6647e69c402a5090fddf285ee7920d92f1718aa5
Stiven Martinez <stivenmartinez@guruhotel.local>
2022-06-17 14:06:08 -0500

    Only edit option added to modules actions

--- components/builder/interface/config/ModulesConfigActions.tsx
+++ components/builder/interface/config/ModulesConfigActions.tsx
@@ -1,30 +1,65 @@
 import { useClickOutside, useHover } from '@guruhotel/aura-hooks'
 import { Button } from '@guruhotel/aura-ui'
-import { CopyIcon, MoveIcon, Pencil2Icon, TrashIcon } from '@radix-ui/react-icons'
+import {
+  CopyIcon,
+  MoveIcon,
+  Pencil2Icon,
+  TrashIcon,
+} from '@radix-ui/react-icons'
 import { ReactNode, useState } from 'react'
 import Sidebar from '../shared/Sidebar'
 import styles from './Modules.module.scss'
 
 interface ModulesConfigActionsProps {
-  module: ReactNode,
+  module: ReactNode
   config: ReactNode
+  onlyEdit: boolean
 }
 
-export default function ModulesConfigActions({ module, config }: ModulesConfigActionsProps) {
+export default function ModulesConfigActions({
+  module,
+  config,
+  onlyEdit,
+}: ModulesConfigActionsProps) {
   // Set hover to allow edit options
-  const { hovered, ref: hoverRef } = useHover();
+  const { hovered, ref: hoverRef } = useHover()
 
   // Manage state when a module is active
   const [active, setActive] = useState(false)
 
   // Disable active state when click outside module
-  const ref = useClickOutside(() => setActive(false));
+  const ref = useClickOutside(() => setActive(false))
   return (
     <div className={styles.modules} ref={hoverRef}>
-      <div ref={ref} className={hovered || active ? `${styles.options} ${styles.active}` : styles.options}>
-        <Sidebar title='Edit module' position="right" trigger={<Button onClick={() => setActive(true)} type="button"><Pencil2Icon /></Button>}>{config}</Sidebar>
-        <Button type="button"><CopyIcon /></Button>
-        <Button type="button"><TrashIcon /></Button>
+      <div
+        ref={ref}
+        className={
+          hovered || active
+            ? `${styles.options} ${styles.active}`
+            : styles.options
+        }
+      >
+        <Sidebar
+          title="Edit module"
+          position="right"
+          trigger={
+            <Button onClick={() => setActive(true)} type="button">
+              <Pencil2Icon />
+            </Button>
+          }
+        >
+          {config}
+        </Sidebar>
+        {!onlyEdit && (
+          <>
+            <Button type="button">
+              <CopyIcon />
+            </Button>
+            <Button type="button">
+              <TrashIcon />
+            </Button>
+          </>
+        )}
       </div>
       <div>{module}</div>
     </div>
--- components/builder/phoenix/layout/PhoenixLayout.tsx
+++ components/builder/phoenix/layout/PhoenixLayout.tsx
@@ -1,19 +1,26 @@
 import styles from './Layout.module.scss'
-import ModulesConfigActions from '../../interface/config/ModulesConfigActions';
-import PhoenixHeaderConfig from '../modules/header/config/PhoenixHeaderConfig';
-import LayoutPages from 'components/builder/interface/layout/pages/LayoutPages';
-import PhoenixHeaderRender from '../modules/header/config/PhoenixHeaderRender';
-import { Box, useTheme } from '@guruhotel/aura-ui';
+import ModulesConfigActions from '../../interface/config/ModulesConfigActions'
+import PhoenixHeaderConfig from '../modules/header/config/PhoenixHeaderConfig'
+import LayoutPages from 'components/builder/interface/layout/pages/LayoutPages'
+import PhoenixHeaderRender from '../modules/header/config/PhoenixHeaderRender'
+import { Box, useTheme } from '@guruhotel/aura-ui'
 
 export default function PhoenixLayout({ children }: any) {
   const { theme } = useTheme()
   return (
-    <div className={styles.layout} style={{ backgroundColor: theme?.colors?.darkie3.toString() }}>
+    <div
+      className={styles.layout}
+      style={{ backgroundColor: theme?.colors?.darkie3.toString() }}
+    >
       <Box css={{ marginBottom: '12px', marginLeft: '20px' }}>
         <LayoutPages />
       </Box>
       <div className={styles.container}>
-        <ModulesConfigActions module={<PhoenixHeaderRender />} config={<PhoenixHeaderConfig />} />
+        <ModulesConfigActions
+          onlyEdit
+          module={<PhoenixHeaderRender />}
+          config={<PhoenixHeaderConfig />}
+        />
         {children}
       </div>
     </div>
