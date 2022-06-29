856d4029a6cf5a32fbe90af4d5c5e0c93e03ea76
Stiven Martinez <stivenmartinez@guruhotel.local>
2022-06-29 15:35:15 -0500

    Add seo title dinamyc

--- pages/builder/index.tsx
+++ pages/builder/index.tsx
@@ -4,21 +4,27 @@ import Layout from 'components/builder/interface/layout/Layout'
 import PhoenixLayout from 'components/builder/phoenix/layout/PhoenixLayout'
 import Modules from 'components/builder/Modules'
 import BuilderEmpty from 'components/builder/empty/BuilderEmpty'
+import Head from 'next/head'
 
 const BuilderHomePage: NextPage = () => {
   // Get theme
   const { theme, pageId } = useApp()
 
-  // Filter modules by page
-  const filterModulesByPage = theme?.pages?.filter(
-    ({ id }: any) => id === pageId
-  )?.[0]?.modules
+  // Get current page
+  const currentPage = theme?.pages?.find((item: any) => item.id === pageId)
 
   return (
     <Layout page="home">
+      <Head>
+        <title>
+          Builder{' '}
+          {currentPage?.config?.title &&
+            `- ${currentPage?.config?.title || '-'}`}
+        </title>
+      </Head>
       <PhoenixLayout>
-        {filterModulesByPage?.length ? (
-          filterModulesByPage?.map((moduleData: any) => {
+        {currentPage?.modules?.length ? (
+          currentPage?.modules?.map((moduleData: any) => {
             if (!moduleData?.stickToFooter)
               return <Modules key={moduleData?.id} moduleData={moduleData} />
             return null
